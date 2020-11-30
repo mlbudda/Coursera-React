3a6b3989fae8f137d4977e57d3e570ea62ae4ca7
Ignas Markauskas <igniuk@gmail.com>
2020-11-30 17:46:56 +0200

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,45 +1,39 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
   function RenderDish({ dish }) {
-    // if we have dish selected, return
-    if (dish != null)
         return(
-          <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-        );
-    // or return empty div's
-    else
-        return(
-            <div></div>
+          <div className="col-12 col-md-5 m-1">
+            <Card>
+              <CardImg top src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
         );
   }
 
-  function RenderComments({ dish }) {
-    if (dish != null)
+  function RenderComments({ comments }) {
+    if (comments != null)
         return(
-          <div className="comments">
-          <h4>Comments</h4>
-            {dish.comments.map((comment1) => (
-              <p id={comment1.id}>
-
-              {comment1.comment} <br/>
-              
-              -- {comment1.author}, 
-              {new Intl.DateTimeFormat('en-US', 
-              { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment1.date)))} 
-
-              </p>
-              
-            ))}
+          <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+              {comments.map((comment) => {
+                return (
+                  <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', 
+                { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                  </li>
+                );
+              })}
+              </ul>
           </div>
         );
     // or return empty div's
@@ -49,18 +43,29 @@ import { Card, CardImg, CardText, CardBody,
         );
   }
     const DishDetail = (props) => {
-    return (
+    if (props.dish != null)
+      return (
         <div className="container">
           <div className="row">
-            <div  className="col-12 col-md-5 m-1">
-              <RenderDish dish={props.dish} />
-            </div>
-            <div  className="col-12 col-md-5 m-1">
-              <RenderComments dish={props.dish} />
+            <Breadcrumb>
+              <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr />
             </div>
           </div>
+          <div className="row">
+            <RenderDish dish={props.dish} />
+            <RenderComments comments={props.comments} />
+          </div>
         </div>
-    );
+      );
+    else
+        return (
+          <div></div>
+        );
     }
 
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderMenuItem ({dish, onClick1}) {
         return (
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
         );
     }
@@ -18,7 +21,7 @@ import { Card, CardImg, CardImgOverlay,
         const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick1} />
+                    <RenderMenuItem dish={dish} />
                     {/* <RenderMenuItem dish={dish} /> */}
                 </div>
             );
@@ -26,6 +29,16 @@ import { Card, CardImg, CardImgOverlay,
 
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     {menu}
                 </div>
