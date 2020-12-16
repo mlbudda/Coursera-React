32640a9df4e9228cfbbaf1eeed1e4c33be767677
Ignas Markauskas <igniuk@gmail.com>
2020-12-16 18:23:17 +0200

    Fetch from Server

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Form } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { Control, LocalForm, Errors, actions } from 'react-redux-form'
+
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
